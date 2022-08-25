98d61feeea2379934a00400d601be0db6827eb56
isocodes51 <flyingcow51@hotmail.com>
2022-08-26 00:20:13 +0300

    Add project files.

---
+++ ReferansClassTips/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ReferansClassTips
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            Person person2 = new Person();
+
+            person1.FirstName = "Engin";
+            person2 = person1;
+            person1.FirstName = "Derin";
+            Console.WriteLine(person2.FirstName);
+            Customer customer = new Customer();
+            Employee employee = new Employee();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+    }
+
+    class Customer:Person
+    {
+        public string CreditCartNumber { get; set; }
+    }
+    
+    class Employee:Person
+    {
+        public int EmployeeNumber { get; set; }
+    }
+}
